287a3671f17b29beb474a2c93d0254595fa61c5f
JordiOrtega <jimenezjordi020@gmail.com>
2024-05-28 13:37:18 -0700

    Adding Data Layout

---
+++ app/src/main/java/com/example/todolistmvvm/data/TodoRepositoryImpl.kt
@@ -0,0 +1,23 @@
+package com.example.todolistmvvm.data
+
+import kotlinx.coroutines.flow.Flow
+
+class TodoRepositoryImpl(
+    private val dao: TodoDao
+): TodoRepository {
+    override suspend fun insertTodo(todo: Todo) {
+        dao.insertTodo(todo)
+    }
+
+    override suspend fun delete(todo: Todo) {
+        dao.delete(todo)
+    }
+
+    override suspend fun getTodoById(id: Int): Todo? {
+        return dao.getTodoById(id)
+    }
+
+    override fun getTodos(): Flow<List<Todo>> {
+        return dao.getTodos()
+    }
+}
